e8cee01311d4a5a53d0c0f68988fc1aa345db037
CyJay96 <konstantin201586@gmail.com>
2023-11-03 17:17:39 +0300

    add exceptions and handler

--- src/main/java/com/advertisement/advertisementsystem/mapper/CountryMapper.java
+++ src/main/java/com/advertisement/advertisementsystem/mapper/CountryMapper.java
@@ -3,8 +3,12 @@ package com.advertisement.advertisementsystem.mapper;
 import com.advertisement.advertisementsystem.model.dto.request.CountryRequest;
 import com.advertisement.advertisementsystem.model.dto.response.CountryResponse;
 import com.advertisement.advertisementsystem.model.entity.Country;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValueCheckStrategy;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper
 public interface CountryMapper {
@@ -13,4 +17,11 @@ public interface CountryMapper {
     Country toCountry(CountryRequest countryRequest);
 
     CountryResponse toCountryResponse(Country country);
+
+    @BeanMapping(
+            nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+            nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS
+    )
+    @Mapping(target = "id", ignore = true)
+    void updateCountry(CountryRequest countryRequest, @MappingTarget Country country);
 }
--- src/main/java/com/advertisement/advertisementsystem/mapper/LanguageMapper.java
+++ src/main/java/com/advertisement/advertisementsystem/mapper/LanguageMapper.java
@@ -3,8 +3,12 @@ package com.advertisement.advertisementsystem.mapper;
 import com.advertisement.advertisementsystem.model.dto.request.LanguageRequest;
 import com.advertisement.advertisementsystem.model.dto.response.LanguageResponse;
 import com.advertisement.advertisementsystem.model.entity.Language;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValueCheckStrategy;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper
 public interface LanguageMapper {
@@ -13,4 +17,11 @@ public interface LanguageMapper {
     Language toLanguage(LanguageRequest languageRequest);
 
     LanguageResponse toLanguageResponse(Language language);
+
+    @BeanMapping(
+            nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+            nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS
+    )
+    @Mapping(target = "id", ignore = true)
+    void updateLanguage(LanguageRequest languageRequest, @MappingTarget Language language);
 }

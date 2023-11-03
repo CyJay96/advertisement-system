097c9a4b43c64cc7993ea6e548c8de1ede78fdf2
CyJay96 <konstantin201586@gmail.com>
2023-11-03 15:12:28 +0300

    initial commit

---
+++ src/main/java/com/advertisement/advertisementsystem/AdvertisementSystemApplication.java
@@ -0,0 +1,12 @@
+package com.advertisement.advertisementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvertisementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvertisementSystemApplication.class, args);
+	}
+}
